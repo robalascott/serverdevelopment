a2e8f537679689b7fd7d4224de8d7fa2fad86b81
robalascott <robalascott@gmail.com>
2016-11-14 07:57:23 +0100

    Login with null warnings
    Failed login not working

--- Lab1/src/ui/LoginBean.java
+++ Lab1/src/ui/LoginBean.java
@@ -2,8 +2,11 @@ package ui;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import bo.UserHandler;
 
@@ -13,6 +16,9 @@ public class LoginBean implements Serializable {
 	private String password;
 	private String userName;
 	private static final long serialVersionUID = 1L;
+	private final static String errorLogin1 = "Incorrect Username and Password";
+	private final static String errorLogin2 = "Please enter correct Username or Password";
+	
 	public String getPassword() {
 		return password;
 	}
@@ -33,10 +39,19 @@ public class LoginBean implements Serializable {
 	public boolean doLogin(){
 		UserHandler handler = new UserHandler();
 		
-		if(handler.login(userName, password))
+		if(handler.login(userName, password)){
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", userName);
 			return true;
-		else
+		}else{
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage( errorLogin1));
 			return false;
-		
+		}
+	}
+	
+	public boolean doLogout(){
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return false;
 	}
 }
